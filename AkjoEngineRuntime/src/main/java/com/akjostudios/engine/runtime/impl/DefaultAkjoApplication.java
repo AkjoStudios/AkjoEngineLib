16227a9c0d473d5bf398103acb88d71ef3edceda
Akjo <lukas.kueffer@outlook.com>
2025-09-02 23:13:27 +0200

    Stopping default application without a dedicated worker thread

--- AkjoEngineRuntime/src/main/java/com/akjostudios/engine/runtime/impl/DefaultAkjoApplication.java
+++ AkjoEngineRuntime/src/main/java/com/akjostudios/engine/runtime/impl/DefaultAkjoApplication.java
@@ -6,6 +6,6 @@ public class DefaultAkjoApplication extends AkjoApplication {
     @Override
     public void onStart() {
         log.info("🔠 This is the default application. No code will be executed and the engine will shut down.");
-        ctx.threading().runOnWorker(() -> ctx.lifecycle().stopApplication("Default Application"));
+        ctx.lifecycle().stopApplication();
     }
 }
