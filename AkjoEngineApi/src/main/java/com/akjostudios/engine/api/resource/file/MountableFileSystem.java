35b318992a49533dfe21db1dab23212d2191ccd3
Akjo <lukas.kueffer@outlook.com>
2025-09-11 20:07:44 +0200

    Added an api note on MountableFileSystem.mount

--- AkjoEngineApi/src/main/java/com/akjostudios/engine/api/resource/file/MountableFileSystem.java
+++ AkjoEngineApi/src/main/java/com/akjostudios/engine/api/resource/file/MountableFileSystem.java
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.NotNull;
 public interface MountableFileSystem extends FileSystem {
     /**
      * Mounts the given file system as the given alias under the given base path.
+     * @apiNote This method is generally not manually called, as no file system implementations are exported to the application module. Use the pre-defined aliases (assets, etc.) instead.
      */
     void mount(@NotNull String alias, @NotNull FileSystem fs, @NotNull String basePath);
 }
